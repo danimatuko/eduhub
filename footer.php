b7bbb8f035395a7e92f5ae21c5ca799cd3871517
danimatuko <danimatuko@outlook.com>
2023-12-06 15:02:55 +0200

    extract the footer to it's template and add static links

--- footer.php
+++ footer.php
@@ -6,12 +6,77 @@
              </div>
          </div>
 
+         <footer class="site-footer">
+             <div class="container">
+                 <div class="row">
+                     <div class="col-lg-4">
+                         <div class="widget">
+                             <h3>About</h3>
+                             <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
+                                 there live the blind texts.</p>
+                         </div> <!-- /.widget -->
+                         <div class="widget">
+                             <address>43 Raymouth Rd. Baltemoer, <br> London 3910</address>
+                             <ul class="list-unstyled links">
+                                 <li><a href="tel://11234567890">+1(123)-456-7890</a></li>
+                                 <li><a href="tel://11234567890">+1(123)-456-7890</a></li>
+                                 <li><a href="mailto:info@mydomain.com">info@mydomain.com</a></li>
+                             </ul>
+                         </div> <!-- /.widget -->
+                     </div> <!-- /.col-lg-4 -->
+                     <div class="col-lg-4">
+                         <div class="widget">
+                             <h3>Company</h3>
+                             <ul class="list-unstyled float-start links">
+                                 <li><a href="#">About us</a></li>
+                                 <li><a href="#">Services</a></li>
+                                 <li><a href="#">Vision</a></li>
+                                 <li><a href="#">Mission</a></li>
+                                 <li><a href="#">Terms</a></li>
+                                 <li><a href="#">Privacy</a></li>
+                             </ul>
+                             <ul class="list-unstyled float-start links">
+                                 <li><a href="#">Partners</a></li>
+                                 <li><a href="#">Business</a></li>
+                                 <li><a href="#">Careers</a></li>
+                                 <li><a href="#">Blog</a></li>
+                                 <li><a href="#">FAQ</a></li>
+                                 <li><a href="#">Creative</a></li>
+                             </ul>
+                         </div> <!-- /.widget -->
+                     </div> <!-- /.col-lg-4 -->
+                     <div class="col-lg-4">
+                         <div class="widget">
+                             <h3>Navigation</h3>
+                             <ul class="list-unstyled links mb-4">
+                                 <li><a href="#">Our Vision</a></li>
+                                 <li><a href="#">About us</a></li>
+                                 <li><a href="#">Contact us</a></li>
+                             </ul>
 
-         <div class="container mx-auto">
-             <!-- Add footer content such as widgets, copyright information, etc. -->
-             <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. All rights reserved.</p>
-         </div>
-         </footer>
+                             <h3>Social</h3>
+                             <ul class="list-unstyled social">
+                                 <li><a href="#"><span class="icon-instagram"></span></a></li>
+                                 <li><a href="#"><span class="icon-twitter"></span></a></li>
+                                 <li><a href="#"><span class="icon-facebook"></span></a></li>
+                                 <li><a href="#"><span class="icon-linkedin"></span></a></li>
+                                 <li><a href="#"><span class="icon-pinterest"></span></a></li>
+                                 <li><a href="#"><span class="icon-dribbble"></span></a></li>
+                             </ul>
+                         </div> <!-- /.widget -->
+                     </div> <!-- /.col-lg-4 -->
+                 </div> <!-- /.row -->
+
+                 <div class="row mt-5">
+                     <div class="col-12 text-center">
+
+                         <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. All rights reserved.</p>
+
+                     </div>
+                 </div>
+             </div> <!-- /.container -->
+         </footer> <!-- /.site-footer -->
+
 
          <?php wp_footer(); ?>
          </body>
--- page.php
+++ page.php
@@ -1,65 +1,8 @@
 <?php get_header(); ?>
 
 
-<div class="site-mobile-menu site-navbar-target">
-    <div class="site-mobile-menu-header">
-        <div class="site-mobile-menu-close">
-            <span class="icofont-close js-menu-toggle"></span>
-        </div>
-    </div>
-    <div class="site-mobile-menu-body"></div>
-</div>
+<?php get_template_part("partials/shared/nav") ?>
 
-<nav class="site-nav">
-    <div class="container">
-        <div class="menu-bg-wrap">
-            <div class="site-navigation">
-                <div class="row g-0 align-items-center">
-                    <div class="col-2">
-                        <a href="index.html" class="logo m-0 float-start">Financing<span class="text-primary">.</span></a>
-                    </div>
-                    <div class="col-8 text-center ">
-                        <ul class="js-clone-nav d-none d-lg-inline-block text-start site-menu mx-auto">
-                            <li><a href="index.html">Home</a></li>
-                            <li class="has-children">
-                                <a href="financing.html">Pages</a>
-                                <ul class="dropdown">
-                                    <li><a href="financing.html">Financing</a></li>
-                                    <li class="active"><a href="single.html">Blog Single</a></li>
-                                    <li><a href="case-study.html">Case Study Detail</a></li>
-                                    <li><a href="#">Menu One</a></li>
-                                    <li><a href="#">Menu Two</a></li>
-                                    <li class="has-children">
-                                        <a href="#">Dropdown</a>
-                                        <ul class="dropdown">
-                                            <li><a href="#">Sub Menu One</a></li>
-                                            <li><a href="#">Sub Menu Two</a></li>
-                                            <li><a href="#">Sub Menu Three</a></li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li class="active"><a href="blog.html">Blog</a></li>
-                            <li><a href="services.html">Services</a></li>
-                            <li><a href="about.html">About</a></li>
-                            <li><a href="contact.html">Contact Us</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-2 text-end">
-                        <a href="#" class="burger ms-auto float-end site-menu-toggle js-menu-toggle d-inline-block d-lg-none light">
-                            <span></span>
-                        </a>
-
-                        <a href="#" class="call-us d-flex align-items-center">
-                            <span class="icon-phone"></span>
-                            <span>123-489-9381</span>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</nav>
 
 <div class="hero overlay inner-page">
     <img src="<?php echo get_theme_file_uri("/images/blob.svg") ?>" alt="" class="img-fluid blob">
@@ -299,85 +242,6 @@
         </div>
     </div>
 </div>
-
-<div class="site-footer">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-4">
-                <div class="widget">
-                    <h3>About</h3>
-                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there
-                        live the blind texts.</p>
-                </div> <!-- /.widget -->
-                <div class="widget">
-                    <address>43 Raymouth Rd. Baltemoer, <br> London 3910</address>
-                    <ul class="list-unstyled links">
-                        <li><a href="tel://11234567890">+1(123)-456-7890</a></li>
-                        <li><a href="tel://11234567890">+1(123)-456-7890</a></li>
-                        <li><a href="mailto:info@mydomain.com">info@mydomain.com</a></li>
-                    </ul>
-                </div> <!-- /.widget -->
-            </div> <!-- /.col-lg-4 -->
-            <div class="col-lg-4">
-                <div class="widget">
-                    <h3>Company</h3>
-                    <ul class="list-unstyled float-start links">
-                        <li><a href="#">About us</a></li>
-                        <li><a href="#">Services</a></li>
-                        <li><a href="#">Vision</a></li>
-                        <li><a href="#">Mission</a></li>
-                        <li><a href="#">Terms</a></li>
-                        <li><a href="#">Privacy</a></li>
-                    </ul>
-                    <ul class="list-unstyled float-start links">
-                        <li><a href="#">Partners</a></li>
-                        <li><a href="#">Business</a></li>
-                        <li><a href="#">Careers</a></li>
-                        <li><a href="#">Blog</a></li>
-                        <li><a href="#">FAQ</a></li>
-                        <li><a href="#">Creative</a></li>
-                    </ul>
-                </div> <!-- /.widget -->
-            </div> <!-- /.col-lg-4 -->
-            <div class="col-lg-4">
-                <div class="widget">
-                    <h3>Navigation</h3>
-                    <ul class="list-unstyled links mb-4">
-                        <li><a href="#">Our Vision</a></li>
-                        <li><a href="#">About us</a></li>
-                        <li><a href="#">Contact us</a></li>
-                    </ul>
-
-                    <h3>Social</h3>
-                    <ul class="list-unstyled social">
-                        <li><a href="#"><span class="icon-instagram"></span></a></li>
-                        <li><a href="#"><span class="icon-twitter"></span></a></li>
-                        <li><a href="#"><span class="icon-facebook"></span></a></li>
-                        <li><a href="#"><span class="icon-linkedin"></span></a></li>
-                        <li><a href="#"><span class="icon-pinterest"></span></a></li>
-                        <li><a href="#"><span class="icon-dribbble"></span></a></li>
-                    </ul>
-                </div> <!-- /.widget -->
-            </div> <!-- /.col-lg-4 -->
-        </div> <!-- /.row -->
-
-        <div class="row mt-5">
-            <div class="col-12 text-center">
-                <!-- 
-              **==========
-              NOTE: 
-              Please don't remove this copyright link unless you buy the license here https://untree.co/license/  
-              **==========
-            -->
-
-                <p>Copyright &copy;<script>
-                        document.write(new Date().getFullYear());
-                    </script>. All Rights Reserved. &mdash; Designed with love by <a href="https://untree.co">Untree.co</a> <!-- License information: https://untree.co/license/ -->
-                </p>
-            </div>
-        </div>
-    </div> <!-- /.container -->
-</div> <!-- /.site-footer -->
 
 
 
