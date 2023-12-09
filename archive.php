eba60eb20a54603af25f000d4a5fe41b9f7eb0a0
danimatuko <danimatuko@outlook.com>
2023-12-09 18:47:03 +0200

    add the archive template

---
+++ archive.php
@@ -0,0 +1,84 @@
+<?php get_header() ?>
+
+<?php get_template_part("partials/shared/nav"); ?>
+
+<?php get_template_part("partials/shared/hero", "inner") ?>
+
+<div class="section sec-news">
+    <div class="container">
+
+        <div class="row">
+            <?php
+
+            if (have_posts()) :  while (have_posts()) : the_post(); ?>
+
+            <div class="col-md-6 col-lg-4 mb-4" data-aos="fade-up" data-aos-delay="0">
+                <div class="card post-entry">
+
+                    <?php if (has_post_thumbnail()) : ?>
+                    <a href="<?php the_permalink(); ?>"><img src="<?php echo get_the_post_thumbnail_url(); ?>"
+                            class="card-img-top" alt="<?php the_title_attribute(); ?>"></a>
+                    <?php endif; ?>
+
+                    <div class="card-body">
+                        <div>
+                            <div class="post-meta d-flex">
+                                <span
+                                    class="text-uppercase font-weight-bold date me-1"><?php echo get_the_date('M d, Y'); ?></span>
+                                <span class="author"><a
+                                        href="<?php echo esc_url(get_author_posts_url(get_the_author_meta('ID'))); ?>"><?php the_author(); ?></a></span>
+                            </div>
+
+                            <h2 class="card-title">
+                                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                            </h2>
+
+
+
+                        </div>
+                        <p><?php the_excerpt(); ?></p>
+                        <?php
+                                $categories = get_the_category();
+                                if (!empty($categories)) {
+                                    foreach ($categories as $category) {
+                                        echo '<span class="badge bg-secondary me-1"><a class="text-white" href="' . esc_url(get_category_link($category->term_id)) . '">' . esc_html($category->name) . '</a></span>';
+                                    }
+                                }
+                                ?>
+                        <p class="mt-5 mb-0"><a href="<?php the_permalink(); ?>">Read more</a></p>
+                    </div>
+                </div>
+            </div>
+
+            <?php endwhile;
+
+            else :
+                // If no posts are found, display a message
+                echo '<p>No posts found</p>';
+            endif;
+            ?>
+
+        </div>
+
+
+
+    </div>
+
+    <div class="row">
+        <div class="col-lg-12 text-center py-5">
+            <div class="custom-navigation">
+                <a href="#" class="active">1</a>
+                <a href="#">2</a>
+                <a href="#">3</a>
+                <a href="#">4</a>
+                <span>...</span>
+                <a href="#">5</a>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+
+
+
+<?php get_footer() ?>
--- partials/shared/hero-inner.php
+++ partials/shared/hero-inner.php
@@ -1,12 +1,58 @@
+<?php
+
+/**
+ * Inner Page Template
+ * 
+ * This template is designed for inner pages such as single posts, category archives, and author archives.
+ * It includes a hero section with a blob image, meta information for single posts, dynamic title display based
+ * on the type of page, and an archive description if available.
+ */
+?>
 <div class="hero overlay inner-page">
+    <!-- Blob Image -->
     <img src="<?php echo get_theme_file_uri("/images/blob.svg") ?>" alt="" class="img-fluid blob">
+
     <div class="container">
         <div class="row align-items-center justify-content-center text-center pt-5">
             <div class="col-lg-6">
-                <p data-aos="fade-up" class="meta">by <a href="#"><?php the_author() ?></a> &bullet; on <a
-                        href="#"><?php the_date() ?></a>
-                </p>
-                <h1 class="heading text-white mb-3" data-aos="fade-up" data-aos-delay="100"><?php the_title() ?></h1>
+                <!-- Meta Information for Single Post -->
+                <?php if (is_single()) : ?>
+                    <p data-aos="fade-up" class="meta">
+                        Posted by
+                        <a href="#"><?php get_the_author() ?></a>
+                        &bullet; on <a href="#"><?php echo get_the_date('M d, Y'); ?></a>
+                    </p>
+                <?php endif ?>
+
+                <!-- Title Display -->
+                <h1 class="heading text-white mb-3" data-aos="fade-up" data-aos-delay="100">
+                    <?php
+                    // Display appropriate title based on page type
+                    if (is_singular()) {
+                        the_title();
+                    } elseif (is_category()) {
+                        single_cat_title();
+                    } elseif (is_author()) {
+                        the_author();
+                    } elseif (is_search()) {
+                        echo 'Search Results for: ' . get_search_query();
+                    } elseif (is_404()) {
+                        echo '404 - Page Not Found';
+                    } elseif (is_home()) {
+                        echo 'Blog';
+                    }
+                    ?>
+                </h1>
+
+                <!-- Archive Description -->
+                <?php
+                $archive_description = get_the_archive_description();
+
+                // Output archive description if it exists
+                if ($archive_description) :
+                    echo '<p class="fs-4">' . $archive_description . '</p>';
+                endif;
+                ?>
             </div>
         </div>
     </div>
